f96c4e254bceab8100f891e8bd90eba84d75aef7
김서현 <kim9905177@gmail.com>
2023-07-10 16:42:30 +0900

    ✨Feat(ch04) 확인문제 4번

---
+++ ThisisJava/src/main/java/org/seohyun/ch04/ex04.java
@@ -0,0 +1,15 @@
+
+public class ex04 {
+    public static void main(String[] args) {
+
+        while (true) {
+            int random1 = (int)(Math.random()*6)+1;
+            int random2 = (int)(Math.random()*6)+1;
+
+            System.out.println("(" + random1 + "," + random2 + ")");
+            if(random1+random2==5){
+                break;
+            }
+        }
+    }
+}
