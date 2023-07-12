4044aeb51e0edcf47969a92b63a1fb15f8eceee3
김서현 <kim9905177@gmail.com>
2023-07-12 17:35:23 +0900

    ✨Feat(ch06) 확인문제 14번

---
+++ src/ch06/Ex14.java
@@ -0,0 +1,13 @@
+package ch06;
+
+public class Ex14 {
+    String name;
+    String id;
+    String password;
+    int age;
+
+    public Ex14(String name, String id){
+        this.name = name;
+        this.id = id;
+    }
+}
