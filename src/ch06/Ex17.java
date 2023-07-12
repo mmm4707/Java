b907fa3c8d8f9734ed8936f5b149d72c6d5401b5
김서현 <kim9905177@gmail.com>
2023-07-12 17:35:47 +0900

    ✨Feat(ch06) 확인문제 17번

---
+++ src/ch06/Ex17.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class Ex17 {
+    public static void println(int value){
+        System.out.println(value);
+    }
+    public static void println(boolean value){
+        System.out.println(value);
+    }
+    public static void println(double value){
+        System.out.println(value);
+    }
+    public static void println(String value){
+        System.out.println(value);
+    }
+}
