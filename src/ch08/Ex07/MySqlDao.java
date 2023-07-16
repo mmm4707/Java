d5763fe3ff7389863848392f00e56cda7d554afb
김서현 <kim9905177@gmail.com>
2023-07-16 22:28:40 +0900

    ✨Feat(ch08) 확인문제 7번 MySqlDao

---
+++ src/ch08/Ex07/MySqlDao.java
@@ -0,0 +1,23 @@
+package ch08.Ex07;
+
+public class MySqlDao implements DataAccessObject{
+    @Override
+    public void select() {
+        System.out.println("MySql DB에서 검색");
+    }
+
+    @Override
+    public void insert() {
+        System.out.println("MySql DB에서 삽입");
+    }
+
+    @Override
+    public void update() {
+        System.out.println("MySql DB에서 수정");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("MySql DB에서 삭제");
+    }
+}
