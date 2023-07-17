de998f4b34001622a24cc369ab9a27c56d9f980d
김서현 <kim9905177@gmail.com>
2023-07-17 16:47:16 +0900

    ✨Feat(ch09) InstanceMemberClass.AExample

---
+++ src/ch09/InstanceMemberClass/AExample.java
@@ -0,0 +1,11 @@
+package ch09.InstanceMemberClass;
+
+public class AExample {
+    public static void main(String[] args) {
+        // A 객체 생성
+        A a = new A();
+
+        // A 인스턴스 메소드 호출
+        a.useB();
+    }
+}
