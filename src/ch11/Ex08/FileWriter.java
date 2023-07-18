0b803754bf091bb1d392d4e73b8c9c8944706f39
김서현 <kim9905177@gmail.com>
2023-07-18 18:32:51 +0900

    ✨Feat(ch11)확인문제 8번 FileWriter

---
+++ src/ch11/Ex08/FileWriter.java
@@ -0,0 +1,17 @@
+package ch11.Ex08;
+
+import java.io.IOException;
+
+public class FileWriter implements AutoCloseable{
+    public FileWriter(String filePath) throws IOException {
+        System.out.println(filePath + " 파일을 엽니다.");
+    }
+
+    public void write(String data) throws IOException{
+        System.out.println(data + "를 파일에 저장합니다.");
+    }
+    @Override
+    public void close() throws Exception {
+        System.out.println("파일을 닫습니다.");
+    }
+}
