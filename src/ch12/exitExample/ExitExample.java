a7d7c2746617c6739c0309e6ac384f1937880d50
김서현 <kim9905177@gmail.com>
2023-07-21 15:17:17 +0900

    ✨Feat(ch12) ExitExample

---
+++ src/ch12/exitExample/ExitExample.java
@@ -0,0 +1,14 @@
+package ch12.exitExample;
+
+public class ExitExample {
+    public static void main(String[] args) {
+        for(int i=0; i<10; i++) {
+            System.out.println(i);
+            if(i == 5) {
+                //JVM 프로세스 종료
+                System.out.println("프로세스 강제 종료");
+                System.exit(0);
+            }
+        }
+    }
+}
